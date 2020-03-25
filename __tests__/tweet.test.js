d891f468c2dc52be791a6de7226c92f134fcc828
surelis segarra <sdarianne34@gmail.com>
2020-03-24 21:13:09 -0700

    deletes tweet by id

--- __tests__/tweet.test.js
+++ __tests__/tweet.test.js
@@ -69,4 +69,19 @@ describe('app routes', () => {
         });
       });
   });
+  it('deletes tweet by id', async() => {
+    const tweet = await Tweet.create(
+      { handle: '@anotherUser', text: 'another cool tweet' });
+
+    return request(app)
+      .delete(`/api/v1/tweets/${tweet._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: tweet._id.toString(),
+          handle: tweet.handle,
+          text: tweet.text,
+          __v: 0
+        });
+      });
+  });
 });
